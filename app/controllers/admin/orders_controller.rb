0ce5294d24f7b3e95054eb5994db717aff828069
kumasakaGit <git@github.com:kumasakaGit/ruby.git>
2023-07-24 15:10:14 +0000

    layout finish

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -2,5 +2,9 @@ class Admin::OrdersController < ApplicationController
   before_action :authenticate_admin!
   def show
     @order = Order.find(params[:id])
+    @total = 0
+    @order.order_items.each do |order_item|
+      @total += order_item.subtotal
+    end
   end
 end
